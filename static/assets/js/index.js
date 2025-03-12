230351033931db1fb97f669f8c2607ec2e451ff7
Travis Scott Fish Official シ <ozzycouts2@gmail.com>
2025-03-11 21:09:12 -0500

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", async (event) => {
     scope: '/assignments/',
   }).then(() => {
     let url = input.value.toLowerCase().trim();
-    if (!isUrl(url)) url = "https://duckduckgo.com/?q=" + url;
+    if (!isUrl(url)) url = "https://search.yahoo.com/search?p=" + url;
     else if (!(url.startsWith("https://") || url.startsWith("http://"))) url = "http://" + url;
     localStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
     location.href = "/mastery";
