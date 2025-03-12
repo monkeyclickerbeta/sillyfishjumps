1ed82faf54d0ed22e64f478cb7cbc55286db7ac0
Travis Scott Fish Official シ <ozzycouts2@gmail.com>
2025-03-12 07:30:49 -0500

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", async (event) => {
     scope: '/assignments/',
   }).then(() => {
     let url = input.value.toLowerCase().trim();
-    if (!isUrl(url)) url = "https://search.yahoo.com/search?p=" + url;
+    if (!isUrl(url)) url = "https://search.yahoo.com/search?q=" + url;
     else if (!(url.startsWith("https://") || url.startsWith("http://"))) url = "http://" + url;
     localStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
     location.href = "/mastery";
