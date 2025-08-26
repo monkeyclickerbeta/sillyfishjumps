0dfa0d6856ff76c4576d25eedee633fdf586ffd0
monkeyclicker <ozzycouts2@gmail.com>
2025-08-25 19:33:34 -0700

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,30 +1,19 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   window.navigator.serviceWorker.register("/lab.js", {
     scope: '/assignments/',
   }).then(() => {
-    let inputValue = input.value.toLowerCase().trim();
-    let url;
-
-    if (!isUrl(inputValue)) {
-      // Redirect to the specified website for non-URL input
-      url = "https://arcticmathurlonly.vercel.app";
-    } else if (!(inputValue.startsWith("https://") || inputValue.startsWith("http://"))) {
-      // Handle URL without protocol
-      url = "http://" + inputValue;
-    } else {
-      // Handle valid URL
-      url = inputValue;
-    }
-
+    let url = input.value.toLowerCase().trim();
+    if (!isUrl(url)) url = "https://search.duckduckgo.com/search?q=" + url;
+    else if (!(url.startsWith("https://") || url.startsWith("http://"))) url = "http://" + url;
     localStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
     location.href = "/mastery";
   });
 });
 
 function isUrl(val = "") {
-  return /^http(s?):\/\//.test(val) || (val.includes(".") && val.substr(0, 1) !== " ");
+  if (/^http(s?):\/\//.test(val) || (val.includes(".") && val.substr(0, 1) !== " ")) return true;
+  return false;
 }
